f1eebfdfcd98081d4a6f5e0eb240f1d024f2c188
Teby <tebygranja@gmail.com>
2023-01-16 12:08:24 -0300

    Database added

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -79,10 +79,19 @@ WSGI_APPLICATION = 'webplayground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# SQL CONECTION
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.mysql',
+      'HOST': 'localhost', 
+      'PORT': '3306',       
+      'USER': 'root',        
+      'PASSWORD': 'agente07', 
+      'NAME': 'playground',
+      'OPTIONS': {
+       'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+       # avoid warning
+      }
     }
 }
 
